a78412f71cbd7c3c70b6111e49474fb2d83d6956
salam46khan <salam46.khan@gmail.com>
2023-12-25 02:09:22 +0600

    add some api for user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -29,13 +29,53 @@ async function run() {
     await client.connect();
 
     const storyCollection = client.db('subornaDB').collection('story');
-    
+    const userCollection = client.db('subornaDB').collection('users');
 
 
     app.get('/story', async (req, res) => {
-        const result = await storyCollection.find().sort({ date: -1 }).toArray()
-        res.send(result)
+      const result = await storyCollection.find().sort({ date: -1 }).toArray()
+      res.send(result)
     })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/user', async (req, res) => {
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result =await userCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.patch('/user/admin/:id', async (req, res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const update = {
+          $set: {
+              admin : true
+          }
+      }
+      const result = await userCollection.updateOne(filter,update, options)
+      res.send(result)
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -49,10 +89,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send('suborno is running')
+app.get('/', (req, res) => {
+  res.send('suborno is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`suborno is running by ${port}`);
+app.listen(port, () => {
+  console.log(`suborno is running by ${port}`);
 })
